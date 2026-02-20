ef67db0c0c3a7d270efeff125214670a795ca09b
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-20 10:45:25 -0300

    feat: add blackText prop to CalculatorButton and update usage in CalculatorScreen

--- src/presentation/components/CalculatorButton.tsx
+++ src/presentation/components/CalculatorButton.tsx
@@ -8,14 +8,15 @@ interface Props {
   label: string;
   color?: string;
   doubleSize?: boolean;
+  blackText?: boolean;
 }
 
-export const CalculatorButton = ( { label, color = colors.darkGray, doubleSize = false,
+export const CalculatorButton = ( { label, color = colors.darkGray, doubleSize = false, blackText = false
   
   }: Props ) => {
   return (
     <Pressable style={ ( { pressed } ) => ( { ...styles.button, backgroundColor: color, width: (doubleSize) ? 180 : 80 ,opacity: (pressed) ? 0.8 : 1}) }>
-      <Text style={ styles.buttonText }>{label }</Text>
+      <Text style={ { ...styles.buttonText, color: (blackText) ? 'black' : 'white'  }  }>{label }</Text>
           </Pressable>
   );
 };
