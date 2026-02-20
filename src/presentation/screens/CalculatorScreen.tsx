19365cf037fe3445bbefddb7bf07eb3779a1c7da
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-20 10:41:53 -0300

    feat: add CalculatorButton component and update CalculatorScreen layout

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,11 +1,48 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { colors, styles } from '../../config/theme/app-theme';
+import { CalculatorButton } from '../components/CalculatorButton';
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text>CalculatorScreen</Text>
+    <View style={ styles.calculatorContainer }>
+
+      <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
+        <Text style={ styles.mainResult}>1500</Text>
+        <Text style={ styles.subResult}>15</Text>
+      </View>
+
+      <View style={ styles.row}>
+        <CalculatorButton label='C' color={colors.lightGray}/>
+        <CalculatorButton label='+/-' color={colors.lightGray} />
+        <CalculatorButton label='del' color={colors.lightGray} />
+        <CalculatorButton label='/' color={colors.orange} />
+      </View>
+      <View style={ styles.row}>
+        <CalculatorButton label='7' color={colors.darkGray}/>
+        <CalculatorButton label='8' color={colors.darkGray} />
+        <CalculatorButton label='9' color={colors.darkGray} />
+        <CalculatorButton label='÷' color={colors.orange} />
+      </View>
+      <View style={ styles.row}>
+        <CalculatorButton label='4' color={colors.darkGray}/>
+        <CalculatorButton label='5' color={colors.darkGray} />
+        <CalculatorButton label='6' color={colors.darkGray} />
+        <CalculatorButton label='-' color={colors.orange} />
+      </View>
+      <View style={ styles.row}>
+        <CalculatorButton label='1' color={colors.darkGray}/>
+        <CalculatorButton label='2' color={colors.darkGray} />
+        <CalculatorButton label='3' color={colors.darkGray} />
+        <CalculatorButton label='+' color={colors.orange} />
+      </View>
+      <View style={ styles.row}>
+        <CalculatorButton label='0' doubleSize color={colors.darkGray}/>
+        <CalculatorButton label='.' color={colors.darkGray} />
+        <CalculatorButton label='=' color={colors.orange} />
+      </View>
+
     </View>
   );
 };
