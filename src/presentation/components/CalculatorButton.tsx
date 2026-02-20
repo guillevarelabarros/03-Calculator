fe8ad3cd279dc3a51c837d12e88611675d5ac35e
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-20 11:13:34 -0300

    feat: add onPress handlers to CalculatorButton instances in CalculatorScreen

--- src/presentation/components/CalculatorButton.tsx
+++ src/presentation/components/CalculatorButton.tsx
@@ -9,13 +9,20 @@ interface Props {
   color?: string;
   doubleSize?: boolean;
   blackText?: boolean;
+  onPress: () => void;
 }
 
-export const CalculatorButton = ( { label, color = colors.darkGray, doubleSize = false, blackText = false
-  
+export const CalculatorButton = ( {
+  label,
+  color = colors.darkGray,
+  doubleSize = false,
+  blackText = false,
+  onPress,
   }: Props ) => {
   return (
-    <Pressable style={ ( { pressed } ) => ( { ...styles.button, backgroundColor: color, width: (doubleSize) ? 180 : 80 ,opacity: (pressed) ? 0.8 : 1}) }>
+    <Pressable
+      onPress={ ()=> onPress() }
+      style={ ( { pressed } ) => ( { ...styles.button, backgroundColor: color, width: ( doubleSize ) ? 180 : 80, opacity: ( pressed ) ? 0.8 : 1 } ) }>
       <Text style={ { ...styles.buttonText, color: (blackText) ? 'black' : 'white'  }  }>{label }</Text>
           </Pressable>
   );
